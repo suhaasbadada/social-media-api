23adaf8fc6612ff6ef730dfac735e50989802ed9
Suhaas Rao Badada <suhaasbadada@gmail.com>
2025-06-02 23:49:24 -0400

    basic api structure

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
 @app.get("/")
-
 async def root():
     return {"message": "Hello, World!"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data":"This is your posts"}
+
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post":f"title: {payload['title']} content:{payload['content']}"}
